d9ab2bcd2544c6da9d1f8dc83717c52ea6d9010a
minjupgeorge <32439811+minjupgeorge@users.noreply.github.com>
2018-02-15 19:55:24 +0530

    Update LinearSearch.c

--- LinearSearch.c
+++ LinearSearch.c
@@ -6,13 +6,13 @@ int a[10],i,n,m,c=0;
 printf("Enter the size of an array: ");
 scanf("%d",&n);
 printf("Enter the elements of the array: ");
-for(i=0;i<=n-1;i++)
+for(i=0;i<n;i++)
 {
 scanf("%d",&a[i]);
 }
 printf("Enter the number to be search: ");
 scanf("%d",&m);
-for(i=0;i<=n-1;i++)
+for(i=0;i<n;i++)
 {
 if(a[i]==m)
  {
