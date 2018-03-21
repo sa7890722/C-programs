795daa163f8b3e0ef047a15f210f47d128a40b1a
Minju P George <32439811+minjupgeorge@users.noreply.github.com>
2018-03-21 15:57:29 +0530

    Create sort_names_pointers.c

---
+++ Strings/sort_names_pointers.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+
+    #include <string.h>
+    void main()
+
+    {
+        char *name[10], temp[8];
+
+        int i, j, n;
+        printf("Enter the value of n \n");
+        scanf("%d", &n);
+
+        for (i = 0; i < n; i++) 
+        {     printf("Enter the name of student %d:",i+1);
+        name[i]=(char *)malloc(20);//dynamic allocation
+              scanf("%s", name[i]);
+        }
+     for (i = 0; i < n - 1 ; i++)
+
+        {
+
+            for (j = i + 1; j < n; j++)
+
+            {
+
+                if (strcmp(name[i], name[j]) > 0) 
+
+                {
+
+                    strcpy(temp, name[i]);
+
+                    strcpy(name[i], name[j]);
+
+                    strcpy(name[j], temp);
+
+                }
+
+            }
+
+        }
+
+        printf("Input NamestSorted names\n");
+
+        for (i = 0; i < n; i++) 
+
+        printf("\t%s\n", name[i]);
+ 
+    }
