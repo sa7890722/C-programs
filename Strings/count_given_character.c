b838abba633ea28b082b35979ab8e9d0bd1b2aaf
Minju P George <32439811+minjupgeorge@users.noreply.github.com>
2018-03-21 16:02:02 +0530

    Create count_given_character.c

---
+++ Strings/count_given_character.c
@@ -0,0 +1,23 @@
+ #include <stdio.h>
+
+int main() {
+   char s[] = "TajMahal";     // String Given
+   char ch = 'a';             // Character to count
+	
+   int i = 0;
+   int count = 0;             // Counter
+      
+   while(s[i] != '\0') {
+      if(s[i] == ch)
+         count++;
+
+      i++;
+   }
+   
+   if(count > 0) 
+      printf("%c appears %d times in '%s'", ch, count, s);
+   else
+      printf("%c did not appear in %s", ch, s);
+   
+   return 0;
+}
